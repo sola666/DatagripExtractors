cf478ee30f73030ea9b50fb0de3d2eebb2084611
so.la <58261219+sola666@users.noreply.github.com>
2023-07-07 13:03:57 +1000

    Create Py-String-List-SingleQuote.groovy

---
+++ Py-String-List-SingleQuote.groovy
@@ -0,0 +1,56 @@
+/*
+ * Available context bindings:
+ *   COLUMNS     List<DataColumn>
+ *   ROWS        Iterable<DataRow>
+ *   OUT         { append() }
+ *   FORMATTER   { format(row, col); formatValue(Object, col); getTypeName(Object, col); isStringLiteral(Object, col); }
+ *   TRANSPOSED  Boolean
+ * plus ALL_COLUMNS, TABLE, DIALECT
+ *
+ * where:
+ *   DataRow     { rowNumber(); first(); last(); data(): List<Object>; value(column): Object }
+ *   DataColumn  { columnNumber(), name() }
+ */
+
+SEPARATOR = ","
+QUOTE     = "'"
+NEWLINE   = System.getProperty("line.separator")
+INDENT    = "  "
+SUFFIX    = ","
+
+def printRow = { values, valueToString, isLast ->
+  values.eachWithIndex { value, idx ->
+    def str = valueToString(value)
+    OUT.append(QUOTE)
+      .append(str.replace(QUOTE, QUOTE + QUOTE))
+      .append(QUOTE)
+      .append(idx != values.size() - 1 ? SEPARATOR : "")
+  }
+  OUT.append((!isLast ? SUFFIX : "") + NEWLINE + (!isLast ? INDENT : ""))
+}
+
+OUT.append("[")
+
+if (!TRANSPOSED) {
+  def iterator = ROWS.iterator()
+  def row = iterator.hasNext() ? iterator.next() : null
+  while (row != null) {
+    def nextRow = iterator.hasNext() ? iterator.next() : null
+    printRow(COLUMNS, { FORMATTER.format(row, it) }, nextRow == null)
+    row = nextRow
+  }
+}
+else {
+  def values = COLUMNS.collect { new ArrayList<String>() }
+  def iterator = ROWS.iterator()
+  def row = iterator.hasNext() ? iterator.next() : null
+  while (row != null) {
+    COLUMNS.eachWithIndex { col, i -> values[i].add(FORMATTER.format(row, col)) }
+    row = iterator.hasNext() ? iterator.next() : null
+  }
+  values.eachWithIndex { value, idx ->
+    printRow(value, { it }, idx == values.size() - 1)
+  }
+}
+
+OUT.append("]")
